14d9127d1ebff34296d440e99abadd5a33a79ed2
robinhhu <emeraldhu@icloud.com>
2023-05-14 10:30:35 -0500

    py task3 buggy results

--- milestone3/python/script_to_all.py
+++ milestone3/python/script_to_all.py
@@ -1,91 +1,95 @@
 import json
+import os
 
-# read the JSON data from file
-with open('dataset/python_all.json', 'r') as file:
-    data = json.load(file)
+temp = ["00","02","03","04","05","06","08","09","12","14","16","20","21","22","23","25","26","27","28","29","30","33","34","38","39","40"]
 
-temp = ['0','1','2','3','4','5','6','7','8','9','10','11','12']
+import openai
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 
-for key,value in data.items():
- if key not in temp:
-    with open('chat_history/'+key+'.md', 'w') as file:
-        if key == "11":
+for iii in temp:
+    folderPath = 'generated_code/code_' + iii +'/mutants'
+    fileList = os.listdir(folderPath)
+    count = 0
+    for f in fileList:
+        count += 1
+        if f == ".DS_Store":
+            count -= 1
             continue
-        prompts = []
+        # read the JSON data from file
+        with open(os.path.join(folderPath,f), 'r',encoding='gbk') as file:
+            data = file.read()
 
-        # prompts.append(
-        # '''Now with the following code
-        # ```python
-        # %s
-        # ```
-        # Can you give me a reasonable unit test for the method?'''
-        # % (original_string))
+            prompts = []
 
-        prompts.append(
-        '''
-        Can you a line by line reasoning of the code below
-        ```python
-        %s
-        ```
-        '''
-        % (value))
+            prompts.append(
+            '''Is the following Python code buggy? Try to reason the code first. The bugs can be in different forms, for example, replaced arithmatic, replaced return value, replaced conditional boundary,etc.
+            ```python
+            %s
+            ```
+            '''
+            % (data))
 
-        prompts.append(
-        '''
-        Now change the data flow of the code, and provides me with a line by line reasoning of the new code. When changing the data flow, the output is not necessarily to keep unchanged. You can add arithmatic operations to the original data, but remember only change the original function, don't use external libraries or create new classes.
-        ''')
+            prompts.append(
+            '''
+            Given the fact that the following Python code is buggy, Can you spot the statements involved in the bug? The bugs can be in different forms, for example, replaced arithmatic, replaced return value, replaced conditional boundary,etc. I care about the correctness.
+            ```python
+            %s
+            ```
+            '''
+            % (data))
 
-        prompts.append(
-        '''
-        Now change the control flow of the code, and provides me with a line by line reasoning of the new code. When chaning the control flow, the output is not necessarily to keep unchanged. Remember only change the original function, don't use any external libraries or create new classes.
-        ''')
 
-        import openai
+            import openai
 
-        # Replace "your_api_key_here" with your actual API key
-        openai.api_key = "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"
+            # Replace "your_api_key_here" with your actual API key
+            openai.api_key = "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"
 
-        # Initialize an empty list to store conversation history
-        conversation_history = []
+            # Initialize an empty list to store conversation history
+            conversation_history = []
 
-        def send_message_to_chatgpt(message):
-            # conversation_history.append({'role': 'system', 'content': 'start the chat'})
-            conversation_history.append({'role': 'user', 'content': message})
+            @retry(wait=wait_random_exponential(min=1,max=60),stop=stop_after_attempt(6))
+            def send_message_to_chatgpt(message):
+                # conversation_history.append({'role': 'system', 'content': 'start the chat'})
+                conversation_history.append({'role': 'user', 'content': message})
 
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                           {'role': 'system', 'content': 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.'}
-                       ] + conversation_history
-            )
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                               {'role': 'system', 'content': 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.'}
+                           ] + conversation_history
+                )
 
-            assistant_message = response.choices[0].message.content
-            conversation_history.append({'role': 'assistant', 'content': assistant_message})
-            return assistant_message
+                assistant_message = response.choices[0].message.content
+                conversation_history.append({'role': 'assistant', 'content': assistant_message})
+                return assistant_message
 
-        # Example usage of the send_message_to_chatgpt function
-        # import pyperclip
-        chat = {}
-        chatstr = ""
-        for i,q in enumerate(prompts):
+            # Example usage of the send_message_to_chatgpt function
+            # import pyperclip
+            chat = {}
+            chatstr = ""
+            for i,q in enumerate(prompts):
 
-            chat.update([("Q" + str(i), q)])
-            chatstr += "\n\n## user: \n\n" + q
-            # pyperclip.copy(chatstr)
+                chat.update([("Q" + str(i), q)])
+                chatstr += "\n\n## user: \n\n" + q
+                # pyperclip.copy(chatstr)
 
-            print("\n\n## user: \n\n", q)
+                print("\n\n## user: \n\n", q)
 
-            a = send_message_to_chatgpt(q)
+                a = send_message_to_chatgpt(q)
 
-            chat.update([("A" + str(i), a)])
-            chatstr += "\n\n## chatgpt: \n\n"+ a
-            print("\n\n## chatgpt: \n\n", a)
+                chat.update([("A" + str(i), a)])
+                chatstr += "\n\n## chatgpt: \n\n"+ a
+                print("\n\n## chatgpt: \n\n", a)
 
-        # with open('5_r.json', 'w') as file:
-        #     json.dump(chat, file, indent=4)
+            with open('buggy_results/code'+iii+'/'+str(count)+'.md', 'w') as file:
+                file.write(chatstr)
 
-        #save chatstr to markdown file
-        file.write(chatstr)
+            #save chatstr to markdown file
+            # file.write(chatstr)
 
-    # with open('chat_history/10.md', 'w') as file:
-    #     file.write(chatstr)
+        # with open('chat_history/10.md', 'w') as file:
+        #     file.write(chatstr)
